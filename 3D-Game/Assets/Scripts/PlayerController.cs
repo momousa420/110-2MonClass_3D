5ecf429de89a33173b9ce82d523e8dc0faa9c2d0
momousa420 <mimity34@gmail.com>
2022-03-28 15:33:43 +0800

    220328
    粒子系統、音效、動畫控制

--- 3D-Game/Assets/Scripts/PlayerController.cs
+++ 3D-Game/Assets/Scripts/PlayerController.cs
@@ -10,8 +10,11 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier = 1;   //系統物理重力
     public bool isOnGround = true;   //是否站在地板上
     public bool gameOver = false;
+    public ParticleSystem pl_Explosion;
+    public ParticleSystem pl_Dirty;
 
     private Animator plAnim;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -24,11 +27,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
             isOnGround = false;
             plAnim.SetTrigger("Jump_trig");
+            pl_Dirty.Stop();
 
         }
     }
@@ -38,11 +42,18 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            pl_Dirty.Play();
         }
         else if (other.gameObject.CompareTag("Obstracte"))
         {
             gameOver = true;
-            print("遊戲結束!");
+
+            //print("遊戲結束!");
+            pl_Explosion.Play();
+
+            plAnim.SetBool("Death_b", true);
+            plAnim.SetInteger("DeathType_int", 1);
+            pl_Dirty.Stop();
         }
             
         }       
